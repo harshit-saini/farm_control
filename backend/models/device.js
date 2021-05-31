8685b2e75ff59d0434fc1be3df07b5eca9c79250
harshit-saini <harshitr.x.t.10@gmail.com>
2021-05-31 14:58:06 +0530

    created models

---
+++ backend/models/device.js
@@ -0,0 +1,44 @@
+/*
+name
+_id
+isPumpOn
+moistureLevel
+updatedAt
+physcialNumber
+*/
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DeviceSchema = new Schema({
+  name: {
+    type: String
+  },
+  moistureLevel: {
+    type: Number
+  },
+  updatedAt: {
+    type: Date
+  },
+  isPumpOn: {
+    type: Boolean,
+    default: false
+  },
+  physicalID: {
+    type: String,
+  },
+  isAssigned: {
+    type: Boolean,
+    default: false
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+})
+
+const Device = mongoose.model("Device", DeviceSchema);
+module.exports = {
+  Device,
+  DeviceSchema
+};
---
+++ backend/models/user.js
@@ -0,0 +1,26 @@
+// farmer is the user
+
+const mongoose = require("mongoose")
+const { DeviceSchema } = require("../models/device")
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  googleID: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  profileImgURL: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  devices: [DeviceSchema]
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User, userSchema };
