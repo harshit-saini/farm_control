0a1c1b2f191e97356c3ee5edb6dee20974498988
harshit-saini <harshitr.x.t.10@gmail.com>
2021-05-31 14:56:55 +0530

    created the server and connected it the database

---
+++ backend/server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express()
+const cors = require("cors");
+const bodyParser = require("body-parser")
+
+const path = require("path")
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const cookieSession = require("cookie-session");
+
+const passport = require("passport");
+const passportSetup = require("./config/passport-setup");
+
+app.use(express.json())
+
+app.use(cors());
+
+
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [process.env.cookieKey]
+}))
+
+passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// database
+const connectDB = require("./config/db")
+connectDB();
+
+
+
+app.use(morgan('dev'))
+
+// routes
+app.use("/auth", require("./routes/auth"))
+app.use("/user", require("./routes/user"))
+app.use("/device", require("./routes/device"))
+
+
+
+// sending the frontend app to the browser
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res, next) => {
+    const path_to_root_folder = path.resolve();
+    res.sendFile(path.resolve(path_to_root_folder, "frontend", "dist", "index.html"));
+  })
+} else {
+  app.get("/", (req, res, next) => {
+    res.json({
+      mode: "development",
+      message: "the api is runnig ..."
+    })
+  })
+}
+
+
+
+
+// initialising the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server started at port ${PORT} ...`)
+})
