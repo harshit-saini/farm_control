3808777e7f5a9df66788640f8df9c3f393dd14e0
harshit-saini <harshitr.x.t.10@gmail.com>
2021-05-31 14:59:13 +0530

    created user context

---
+++ frontend/src/contexts/userContext.js
@@ -0,0 +1,46 @@
+import React, { createContext, Component } from 'react'
+import axios from 'axios'
+
+export const UserContext = createContext();
+
+class UserContextProvider extends Component {
+
+  state = {
+    isAuthenticated: false,
+    user: {}
+  }
+
+  componentDidMount() {
+    /*
+    1. fetch the user from the backend by calling endpoint eg : /user
+    2. set the user in state
+    */
+
+    const getData = async () => {
+      try {
+        const { data } = await axios.get("/user");
+        if (data.error) return;
+        const { currentUser } = data;
+        this.setState({ user: currentUser })
+        this.setState({ isAuthenticated: true })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getData();
+
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <UserContext.Provider value={{ ...this.state }} >
+        {children}
+      </UserContext.Provider>
+    )
+  }
+}
+
+export default UserContextProvider;
